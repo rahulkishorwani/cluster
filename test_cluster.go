759b9ff05c2dad58494cba286f77172b55e0bf9f
rahulkishorwani <kivirani@gmail.com>
2014-02-02 01:22:23 +0530

    Create test_cluster.go

---
+++ test_cluster.go
@@ -0,0 +1,43 @@
+package main
+import (
+	//"fmt"
+	"github.com/rahulkishorwani/cluster"
+	"sync"
+	"os"
+	"os/exec"
+	"strconv"
+	"syscall"
+)
+func main() {
+   
+	numberofservers:=0
+	numberofservers,_=strconv.Atoi(os.Args[1])
+	wg := new(sync.WaitGroup)
+    	wg.Add(numberofservers)
+
+	for i:=0;i<numberofservers;i++ {
+		id:=strconv.Itoa(i)
+		go cluster.serverfunc(id,20000,numberofservers,wg)	
+	}
+	
+	wg.Wait()
+
+
+	binary, lookErr := exec.LookPath("bash")
+    	if lookErr != nil {
+    	    panic(lookErr)
+    	}
+	
+
+	args := []string{"bash", "mybash.sh"}
+	env := os.Environ()
+	
+	
+	execErr := syscall.Exec(binary, args, env)
+    	if execErr != nil {
+        	panic(execErr)
+    	}
+
+	
+	
+}
